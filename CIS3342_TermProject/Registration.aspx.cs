e643b98062735fa03f0a5e42537c146d92840420
Frank Yuhas <frankyuhas@icloud.com>
2021-04-14 18:30:44 -0400

    Registration Page
    Completed registration page, added SOAP api and stored procedures to add new user to db

--- CIS3342_TermProject/Registration.aspx.cs
+++ CIS3342_TermProject/Registration.aspx.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using CIS3342_TermProject.Models;
+
+
 
 namespace CIS3342_TermProject
 {
@@ -11,6 +14,40 @@ namespace CIS3342_TermProject
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            DatingAppWebService.User newUser = new DatingAppWebService.User();
+
+            //checks if passwords match, then creates a new user object
+            if (txtPassword.Text == txtReenterPassword.Text)
+            { 
+                
+                newUser.UserName = txtName.Text;
+                newUser.Birthdate = DateTime.Parse(inputDate.Text);
+                newUser.EmailAddress = txtEmail.Text;
+                newUser.PhoneNumber = txtPhone.Text;
+                newUser.Gender = ddlGender.SelectedItem.ToString();
+                newUser.Bio = txtBio.Text;
+                newUser.Location = ddlState.SelectedItem.ToString();
+                newUser.Password = txtPassword.Text;
+            }
+
+            DatingAppWebService.DatingApp proxy = new DatingAppWebService.DatingApp();
+            int result = proxy.AddNewUser(newUser);
+
+
+            if(result == 0)
+            {
+                Response.Write("<h3>Worked?</h3>");
+            }
+            else
+            {
+                Response.Write("<h3>Nope</h3>");
+
+            }
 
         }
     }
